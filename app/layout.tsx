51efb042adc14ab6577943fab39eda39f9e70ef6
Sven Aarts <sven_aarts@epam.com>
2024-10-05 07:11:00 +0200

    add theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next/types";
 import "./layout.css";
+
+export const metadata: Metadata = {
+  themeColor: "#f5f5f5",
+};
 
 export default function RootLayout({
   children,
